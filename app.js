ec86efde06b5fafa8ee8ea551a89097c5f0e8bf7
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2023-02-11 21:16:41 +0400

    change file structure and create seperate folders for routes and controllers

--- app.js
+++ app.js
@@ -1,62 +1,25 @@
-const fs = require('fs');
-const express = require('express');
+const express = require('express')
+const morgan = require('morgan')
+const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(morgan('dev'))
 
-const port = 3000;
-const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'));
+app.use(express.json())
 
-app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
-});
+app.use((req, res, next) => {
+  console.log('Hello from the middleware')
+  next()
+})
 
-app.get('/api/v1/tours/:id', (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString()
+  next()
+})
 
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-});
-
-app.post('/api/v1/tours', (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(
-    './dev-data/data/tours-simple.json',
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-});
-
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app
