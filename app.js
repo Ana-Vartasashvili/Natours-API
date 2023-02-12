f929b926dfda44bac5f0d6b05ef3d2fa051eaba4
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2023-02-12 18:58:15 +0400

    create a env variable for MongoDB database

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const userRouter = require('./routes/userRoutes')
 
 const app = express()
 
-app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use(express.json())
+app.use(express.static('./public'))
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware')
-  next()
-})
+app.use((req, res, next) => console.log('Hello from the middleware'), next())
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,26 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 )
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    })
+  }
+  next()
+}
+
+exports.checkID = (req, res, next, val) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    })
+  }
+  next()
+}
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -17,13 +37,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   const id = +req.params.id
   const tour = tours.find((el) => el.id === id)
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    })
-  }
 
   res.status(200).json({
     status: 'success',
@@ -54,13 +67,6 @@ exports.createTour = (req, res) => {
 }
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    })
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -70,13 +76,6 @@ exports.updateTour = (req, res) => {
 }
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    })
-  }
-
   res.status(204).json({
     status: 'success',
     data: null,
