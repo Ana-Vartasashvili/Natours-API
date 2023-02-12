76bf8824ff365ca69f2f0173f21441cde79ee0bb
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2023-02-12 19:11:04 +0400

    connect the application to MongoDB database

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json())
 app.use(express.static('./public'))
 
-app.use((req, res, next) => console.log('Hello from the middleware'), next())
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
   next()
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const app = require('./app')
 
 dotenv.config({ path: './config.env' })
+
+const DB = process.env.DATABASE
+mongoose
+  .connect(DB, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+  })
+  .then((connection) => console.log(connection.connections))
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`App running on port ${port}...`))
