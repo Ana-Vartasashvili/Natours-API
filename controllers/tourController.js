8c5797ae16273736b7a88d66641b2d34b9f95d8d
Ana-Vartasashvili <anavartasashvili30@gmail.com>
2023-02-13 11:47:35 +0400

    refactor the code structure for MVC

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,42 +1,18 @@
-const fs = require('fs')
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-)
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    })
-  }
-  next()
-}
-
-exports.checkID = (req, res, next, val) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    })
-  }
-  next()
-}
+const Tour = require('../models/tourModel')
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   })
 }
 
 exports.getTour = (req, res) => {
   const id = +req.params.id
-  const tour = tours.find((el) => el.id === id)
+  // const tour = tours.find((el) => el.id === id)
 
   res.status(200).json({
     status: 'success',
@@ -47,23 +23,12 @@ exports.getTour = (req, res) => {
 }
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1
-  const newTour = Object.assign({ id: newId }, req.body)
-
-  tours.push(newTour)
-
-  fs.writeFile(
-    './dev-data/data/tours-simple.json',
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      })
-    }
-  )
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: newTour,
+    },
+  })
 }
 
 exports.updateTour = (req, res) => {
--- server.js
+++ server.js
@@ -5,30 +5,12 @@ const app = require('./app')
 dotenv.config({ path: './config.env' })
 
 const DB = process.env.DATABASE
-mongoose
-  .connect(DB, {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true,
-  })
-  .then((connection) => console.log(connection.connections))
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
+mongoose.connect(DB, {
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
-const Tour = mongoose.model('Tour', tourSchema)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`App running on port ${port}...`))
